8fe450517862e771392695a80ef9d311bdc60991
David-J-Hwang <jeongjaehwang@gmail.com>
2025-03-25 02:00:23 +0900

    Add comments to src/pages/Login.jsx file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,36 +15,50 @@ const Login = () => {
   // handleSignIn: 로그인 처리함수
   const handleSignIn = async (e) => {
     e.preventDefault();
+    // 로그인 과정을 처리하는 동안 버튼을 비활성화하도록 loading state를 true로 둔다
     setLoading(true);
+    // 사용자에게 보일 에러 메시지인 error state를 초기화한다
     setError("");
 
+    // supabase.auth.signInWithPassword() 함수를 사용해 로그인을 요청한다
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
 
+    // 로그인 실패 시 error.message로 error state를 설정한다
     if (error) {
       setError(error.message);
-    } else {
-      navigate("/dashboard"); // 로그인 성공 시 이동
+    }
+    // 로그인 성공 시 Dashboard 페이지로 이동한다
+    else {
+      navigate("/dashboard");
     }
 
+    // 로그인 처리가 끝나면 로딩 상태를 완료로 두기 위해 loading state를 false로 둔다
     setLoading(false);
   };
 
   // handleSignUp: 회원가입 처리함수
   const handleSignUp = async () => {
+    // 로그인 과정을 처리하는 동안 버튼을 비활성화하기 위해 loading state를 true로 둔다다
     setLoading(true);
+    // 사용자에게 보일 에러 메시지인 error state를 초기화한다
     setError("");
 
+    // supabase.auth.signUp() 함수를 사용해 회원가입을 요청한다
     const { error } = await supabase.auth.signUp({ email, password });
 
+    // 회원가입 실패 시 error.message로 error state를 설정한다
     if (error) {
       setError(error.message);
-    } else {
+    }
+    // 회원가입 성공 시 이메일 확인을 요청하는 alert를 표시한다
+    else {
       alert("회원가입 완료! 이메일을 확인해주세요.");
     }
 
+    // 회원가입 처리가 완료된 경우 loading state를 false로 둔다
     setLoading(false);
   };
 
